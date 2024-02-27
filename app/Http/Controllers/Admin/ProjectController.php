c701b628502f4f6066e739977cd5d78bac8a0883
nicomanunta <nic.manun@gmail.com>
2024-02-27 15:52:56 +0100

    recupero i type

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Storage;
+use App\Models\Type;
 
 
 
@@ -33,6 +34,8 @@ class ProjectController extends Controller
      */
     public function create()
     {
+        $types = Type::all();
+        
         return view('admin.projects.create');
     }
 
